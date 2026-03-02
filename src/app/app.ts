cb8e69972fd0d75d1e78a754423702a21be47426
k4y4 <k4y4no@gmail.com>
2026-03-02 23:52:36 +0100

    Setup responsive menu v1.00

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,41 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './parts/navbar/navbar';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    Navbar
+],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit{
   protected readonly title = signal('motorsFront');
+  burgerStatus!: boolean;
+
+  ngOnInit(): void {
+    this.burgerStatus = false;
+  }
+
+
+  drawerStatus(){
+    this.burgerStatus = !this.burgerStatus
+    console.log(this.burgerStatus)
+/*     this.burgerStatus?document.body.classList.add('no-scroll'):document.body.classList.remove('no-scroll')
+ */
+  if (this.burgerStatus) {
+
+    document.body.classList.add('no-scroll');
+  } else {
+
+    setTimeout(() => {
+      document.body.classList.remove('no-scroll');
+    }, 250); 
+  }
+  }
+
 }
